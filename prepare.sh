a2c0edb823fd7ab0a6aa6fb2cb4b669ef9beb897
Onur Eren Arpaci <onurerenarpaci@hotmail.com>
2023-05-21 21:00:15 +0300

    env variabe for opensearch volume path

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,9 @@
+## install aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+
 git clone https://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@github.com/hukuki/haystack.git
 aws configure
 cd haystack
