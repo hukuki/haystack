8bf2f9c4fdd9e82b0a8a90741ab4eabd332f1e78
Onur Eren Arpaci <onurerenarpaci@hotmail.com>
2023-09-02 22:46:20 -0400

    new models

---
+++ pipeline/custom_component.py
@@ -0,0 +1,46 @@
+"""
+Pipelines allow putting together Components to build a graph.
+
+In addition to the standard Haystack Components, custom user-defined Components
+can be used in a Pipeline YAML configuration.
+
+The classes for the Custom Components must be defined in this file.
+"""
+
+
+from haystack.nodes.base import BaseComponent
+from typing import Dict, List, Optional, Union, Iterator
+
+
+class SampleComponent(BaseComponent):
+    outgoing_edges: int = 1
+
+    def run(self, **kwargs):
+        raise NotImplementedError
+    
+class QueryPreprocessor(BaseComponent):
+    outgoing_edges: int = 1
+
+    def run(self, query: Optional[str], **kwargs):
+        import string
+        import re
+        import unicodedata
+        
+        weird_chars_keys = "á×‘ãòŷІ≤ý˂ ≈οà′éȗȋ–—‐а’“š≥É‚êïìŜóú⁄å‒”ô℮è―˃ʺÌ¡„−íäķậēÏ"
+        weird_chars_values = 'ax\'aoyI<y< =oa\'eui---a\'"ş>E,eiiSou/a-"oee->"İi,-iakaeİ'
+        
+        text = unicodedata.normalize('NFKC', text) # Normalize unicode 
+        text = re.sub(r'\s+', ' ', text) # Remove extra spaces
+        text = text.translate(str.maketrans('âîûÂÎÛ', 'aiuAİU')) # Normalize Turkish characters by removing circumflex
+        text = text.translate(str.maketrans(weird_chars_keys, weird_chars_values)) # Fix weird characters
+        text = text.strip() # Remove leading and trailing spaces
+        return text, "output_1"
+    
+    def run_batch(self, queries: Optional[List[str]]):
+        texts = []
+
+        for query in queries:
+            text = self.run(query)
+            texts.append(text[0])
+
+        return texts, "output_1"
